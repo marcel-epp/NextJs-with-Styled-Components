d3ec2e8c342d9bc9bb459874dff081a1956bcb4d
Marcel Epp <epp.marcel@icloud.com>
2024-02-14 12:19:06 +0100

    add basic structure with styled components

---
+++ src/styles/styles.js
@@ -0,0 +1,18 @@
+// styles.js
+import { createGlobalStyle } from "styled-components";
+export default createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+  :root {
+    --primary-background-color: aquamarine;
+  } 
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica,
+      Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  }
+	// more global styles here...
+`;
